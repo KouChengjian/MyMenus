70ac47360c7ad17a9a2c0c15f95fece4a25a4534
kouchengjian <hui793114465@163.com>
2017-05-04 21:43:19 +0800

    add view

--- MyMenu/src/com/jikexueyuan/mymenu/MainActivity.java
+++ MyMenu/src/com/jikexueyuan/mymenu/MainActivity.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.LinearLayout;
 
 public class MainActivity extends FragmentActivity {
 
@@ -16,16 +21,29 @@ public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		menuUI = new MenuUI(this);
-		setContentView(menuUI);
-		leftMenu = new LeftMenu();
-		getSupportFragmentManager().beginTransaction()
-				.add(MainUI.MIDEELE_ID, leftMenu).commit();
+		setContentView(R.layout.activity_main);
+//		menuUI = new MenuUI(this);
+//		setContentView(menuUI);
+//		leftMenu = new LeftMenu();
+//		getSupportFragmentManager().beginTransaction()
+//				.add(MainUI.MIDEELE_ID, leftMenu).commit();
 //		mainUI = new MainUI(this);
 //		setContentView(mainUI);
 //		leftMenu = new LeftMenu();
 //		getSupportFragmentManager().beginTransaction()
 //				.add(MainUI.LEFT_ID, leftMenu).commit();
+		View view = LayoutInflater.from(this).inflate(R.layout.left, null);
+		LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(      
+                LinearLayout.LayoutParams.WRAP_CONTENT,      
+                LinearLayout.LayoutParams.WRAP_CONTENT      
+        );   
+		view.setLayoutParams(p);
+		getViewGroup().addView(view);
 	}
+	
+	private ViewGroup getViewGroup() {
+        return (ViewGroup) this.findViewById(Window.ID_ANDROID_CONTENT);
+        //return (ViewGroup) GSYApplication.getInstance().getWindow().findViewById(Window.ID_ANDROID_CONTENT);
+    }
 
 }
